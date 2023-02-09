f348f268057e5f243be90fbf115519d0115e89c9
itsuki0927 <2309899048@qq.com>
2023-02-09 23:29:01 +0800

    chore: init

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,14 +1,12 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+import { Header } from 'RemixAppHeader';
+import { Footer } from 'RemixAppFooter';
 
 export default function Index() {
-    return (
-        <>
-            <Header/>
-            <div>
-                Content!
-            </div>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div>Content!</div>
+      <Footer />
+    </>
+  );
 }
